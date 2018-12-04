6e18bfc10bc92f054e458068d2c7ba2d356799e7
Elad Gubkin <Eladox3@gmail.com>
2018-12-04 22:23:04 +0200

    Initial express server with route files

---
+++ server/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    'mongodb://Eladox:Eladox123@eladoxcluster-shard-00-00-g25lc.mongodb.net:27017,eladoxcluster-shard-00-01-g25lc.mongodb.net:27017,eladoxcluster-shard-00-02-g25lc.mongodb.net:27017/test?ssl=true&replicaSet=EladoxCluster-shard-0&authSource=admin&retryWrites=true'
+};
---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const users = require('./routes/api/users');
+const profile = require('./routes/api/profile');
+
+const app = express();
+
+// DB Config
+const db = require('./config/keys').mongoURI;
+// Connect to MongoDB
+mongoose
+  .connect(
+    db,
+    { useNewUrlParser: true }
+  )
+  .then(() => console.log('MongoDB Connected!'))
+  .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('Hello World'));
+
+// Use Routes
+app.use('/api/users', users);
+app.use('/api/profile', profile);
+
+const port = process.env.PORT || 5000;
+app.listen(port, () => console.log(`Server started on port ${port}`));
